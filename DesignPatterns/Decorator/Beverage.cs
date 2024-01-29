a6c5bda1f1dc6a719882e1638660124a87278c18
Vincent Terpstra <vdterpst@gmail.com>
2024-01-28 19:40:35 -0500

    cleanup

--- DesignPatterns/Decorator/Beverage.cs
+++ DesignPatterns/Decorator/Beverage.cs
@@ -4,11 +4,10 @@ public abstract class Beverage
 {
     protected readonly string _description;
     protected readonly decimal _cost;
-
-
+    
     protected Beverage(string description, decimal cost)
     {
-        this._description = description;
+        _description = description;
         _cost = cost;
     }
 
