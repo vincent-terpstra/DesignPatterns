5327d011f2870d4d19fd9e6f33313a723b854834
Vincent Terpstra <vdterpst@gmail.com>
2024-01-28 20:57:01 -0500

    Adding Tests for Beverages and Decorator patterns Adding Extension Methods for easily adding condiments

---
+++ DesignPatterns.Test/Decorator/BeveragesShould.cs
@@ -0,0 +1,29 @@
+﻿using DesignPatterns.Decorator;
+using DesignPatterns.Decorator.Beverages;
+
+namespace DesignPatterns.Test.Decorator;
+
+public class BeveragesShould
+{
+    [Fact]
+    public void Soy_Milk_Decaf()
+    {
+        Beverage soy_milk_decaf = new Decaf()
+            .AddMilk()
+            .AddSoy();
+
+        soy_milk_decaf.Cost.Should().Be(2.10m);
+        soy_milk_decaf.Description.Should().Be("Decaf Coffee, Milk, Soy");
+    }
+    
+    [Fact]
+    public void DarkRoast_Mocha_Whip()
+    {
+        Beverage DarkRoast_Mocha_Whip = new DarkRoast()
+            .AddMocha()
+            .AddWhip();
+
+        DarkRoast_Mocha_Whip.Cost.Should().Be(2.20m);
+        DarkRoast_Mocha_Whip.Description.Should().Be("Dark Roast Coffee, Mocha, Whip");
+    }
+}
---
+++ DesignPatterns/Decorator/BeveragesExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Decorator;
+
+public static class BeveragesExtensions
+{
+    public static Beverage AddMilk(this Beverage beverage)
+        => Condiment.Milk(beverage);
+    
+    public static Beverage AddMocha(this Beverage beverage)
+        => Condiment.Mocha(beverage);
+    
+    public static Beverage AddSoy(this Beverage beverage)
+        => Condiment.Soy(beverage);
+    
+    public static Beverage AddWhip(this Beverage beverage)
+        => Condiment.Whip(beverage);
+    
+}
