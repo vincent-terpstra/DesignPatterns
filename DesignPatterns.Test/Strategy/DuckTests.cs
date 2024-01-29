454c42e43299e53a1b6fb91acde18c1c66155371
Vincent Terpstra <vdterpst@gmail.com>
2024-01-28 19:56:36 -0500

    Adding tests for duck class - Duck methods call strategy methods - Duck strategies can be changed

---
+++ DesignPatterns.Test/Strategy/DuckTests.cs
@@ -0,0 +1,44 @@
+﻿using DesignPatterns.Strategy;
+using DesignPatterns.Strategy.Interfaces;
+using Moq;
+
+namespace DesignPatterns.Test.Strategy;
+
+public class DuckTests
+{
+    [Fact]
+    public void Quack_Should_Call_Quack()
+    {
+        // Arrange 
+        var quack = new Mock<IQuackBehaviour>();
+            quack.Setup(q => q.Quack());
+            Duck duck = new Duck(quackBehaviour: quack.Object);
+
+        // Act
+        duck.Quack();
+
+        // Assert
+        quack.Verify(q => q.Quack(), Times.Once);
+    }
+
+    [Fact]
+    public void Swim_Behaviour_Switched()
+    {
+        // Arrange
+        var swim = new Mock<ISwimBehaviour>();
+            swim.Setup(s => s.Swim());
+            
+        var replace = new Mock<ISwimBehaviour>();
+            replace.Setup(s => s.Swim());
+            
+        Duck duck = new Duck(swimBehaviour: swim.Object);
+        
+        // Act
+        duck.SwimBehaviour = replace.Object;
+        duck.Swim();
+        
+        // Assert
+        swim.Verify(s => s.Swim(), Times.Never);
+        replace.Verify(s => s.Swim(), Times.Once);
+    }
+}
