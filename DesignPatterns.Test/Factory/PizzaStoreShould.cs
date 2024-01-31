2677de65873fce537f7e6bbc6d72b10fbe8ad75c
Vincent Terpstra <vdterpst@gmail.com>
2024-01-30 21:07:38 -0500

    Adding tests for pizza factory

---
+++ DesignPatterns.Test/Factory/PizzaStoreShould.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatterns.Factory;
+using DesignPatterns.Factory.Pizzas;
+
+namespace DesignPatterns.Test.Factory;
+
+public class PizzaStoreShould
+{
+    [Fact]
+    public void PreparePizza()
+    {
+        // Arrange
+        PizzaFactory factory = new();
+        PizzaStore store = new(factory);
+
+        // Act
+        var pizza = store.Order(PizzaType.Cheese);
+
+        // Assert
+        pizza.Should().BeOfType<CheesePizza>();
+        pizza.IsReady.Should().BeTrue();
+    }
+}
