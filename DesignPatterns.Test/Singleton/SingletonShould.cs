2112880eab5f822c63c601fe9bd376a05b9b7370
Vincent Terpstra <vdterpst@gmail.com>
2024-01-31 20:51:48 -0500

    Adding SingletonPattern.cs and tests

---
+++ DesignPatterns.Test/Singleton/SingletonShould.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns.Singleton;
+
+namespace DesignPatterns.Test.Singleton;
+
+public class SingletonShould
+{
+    [Fact]
+    public void Only_Create_One()
+    {
+        var singleton1 = SingletonPattern.Instance;
+        var singleton2 = SingletonPattern.Instance;
+
+        ReferenceEquals(singleton1, singleton2).Should().BeTrue();
+    }
+}
---
+++ DesignPatterns/Singleton/SingletonPattern.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.Singleton;
+
+public class SingletonPattern
+{
+    private SingletonPattern()
+    {
+        
+    }
+
+    private static SingletonPattern? _instance;
+    public static SingletonPattern Instance => _instance ??= new();
+}
